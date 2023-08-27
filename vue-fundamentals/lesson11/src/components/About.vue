262e00e4299879729adb4b386cc9f36c15a498e6
griceldavaldez <gvaldez@sodep.com.py>
2023-08-27 19:45:35 -0400

    Transitions & Animations
    -Dynamic components
    -Animating with CSS Transitions
    -Animating with CSS Animations
    -Animating with Javascript
    -Animating with animate.css library

---
+++ vue-fundamentals/lesson11/src/components/About.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1>About Page</h1>
+</template>
+
+<style scoped>
+h1{
+  margin: 5px;
+  color: blue;
+  font-size: 100px;
+  font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+}
+</style>
---
+++ vue-fundamentals/lesson11/src/components/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <h1>Home Page</h1>
+</template>
+
+<script>
+export default {
+  unmounted() {
+    console.log("Home component unmounted");
+  },
+  activated() {
+    console.log("Home component activated");
+  },
+  deactivated() {
+    console.log("Home component deactivated");
+  },
+};
+</script>
+
+<style scoped>
+h1{
+  margin: 5px;
+  color: red;
+  font-size: 100px;
+  font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+}
+</style>
